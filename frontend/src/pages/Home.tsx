2f0711e2b3978e1280118ba60f1a3b1250bc9b0f
Rute Figueiredo <rute.figueiredo92@gmail.com>
2023-05-13 15:18:27 +0100

    started to implement layout page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import styles from './Home.module.scss'
 export function HomePage () {
     return (
         <div className={styles.container}>
-            
+            <h1>User.name</h1>
         </div>
     )
 }
