8002594cea2807be650af9ff4af9bb693db548a4
shadmau <maduakorfelix@gmail.com>
2023-05-14 09:53:41 +0200

    final push

--- contracts/nft-discount-module/script/DeployNFT.sol
+++ contracts/nft-discount-module/script/DeployNFT.sol
@@ -15,11 +15,11 @@ contract DeployerScriptNFT is Script {
     function run() public {
         uint256 deployerPrivKey = vm.envUint("DEPLOYER_PRIV_KEY");
         vm.broadcast(deployerPrivKey);
-        UndeadNFT undead = new UndeadNFT();
+       // UndeadNFT undead = new UndeadNFT();
 
-       /*
-        UndeadNFT undead = UndeadNFT(0x86E192Bd7a223a39a251c42BC7C9ec000F7086Cd);
-        undead.mintNFT(0xd9CdFbBb0dCA90F1367673337F441EcF09172b7B, "https://af1-lookbook.rtfkt.com/assets/images/dna/genesis/apparel/af1_1.jpg");
-        */
+       
+        UndeadNFT undead = UndeadNFT(0x0957b85B61B0824a859Dd44FCBb3153FE6669371);
+        undead.mintNFT(0xd5f005e4eaeaa52d9d2ef05bf9c8f464092c6bdc, "https://af1-lookbook.rtfkt.com/assets/images/dna/genesis/apparel/af1_1.jpg");
+        
     }
 }
---
+++ contracts/nft-discount-module/src/UndeadNFT.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract UndeadNFT is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("NIKE-UNDEAD", "UNDEAD") {}
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+      function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+    
+
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
